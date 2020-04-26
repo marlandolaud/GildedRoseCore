74a9301cfa7d4d95fd8d9813ebc9154c861cd3ff
mlaud <mlaud@outlook.com>
2020-04-26 11:44:49 -0400

    Added back untouchable Items list to Program.cs

--- GildedRose/GildedRose.UI.Home/Program.cs
+++ GildedRose/GildedRose.UI.Home/Program.cs
@@ -6,28 +6,36 @@ namespace GildedRose.UI.Home
 {
     partial class Program
     {
+        /// <summary>
+        /// do not alter
+        /// </summary>
+        IList<Item> Items;
+
         static void Main(string[] args)
         {
             System.Console.WriteLine("OMGHAI!");
 
-            var Items = new List<Item>
+            var app = new Program()
             {
-                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                new Item
-                    {
-                        Name = "Backstage passes to a TAFKAL80ETC concert",
-                        SellIn = 15,
-                        Quality = 20
-                    },
-                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+                Items = new List<Item>
+                {
+                    new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                    new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                    new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                    new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                    new Item
+                        {
+                            Name = "Backstage passes to a TAFKAL80ETC concert",
+                            SellIn = 15,
+                            Quality = 20
+                        },
+                    new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+                }
             };
 
             var service = new ItemQualityService();
 
-            service.UpdateQuality(Items);
+            service.UpdateQuality(app.Items);
 
             System.Console.ReadKey();
         }
