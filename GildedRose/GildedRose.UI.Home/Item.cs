ebeae84e2e8eaacb2c08de88e45cdf7485b6dcf0
mlaud <mlaud@outlook.com>
2020-04-26 10:06:02 -0400

    moved Items and UpdateQuality to their own class

---
+++ GildedRose/GildedRose.UI.Home/Item.cs
@@ -0,0 +1,14 @@
+﻿namespace GildedRose.UI.Home
+{
+    /// <summary>
+    /// Do not alter the class or the Properties inside
+    /// </summary>
+    public class Item
+    {
+        public string Name { get; set; }
+
+        public int SellIn { get; set; }
+
+        public int Quality { get; set; }
+    }
+}
